93b6fe25c81c84ac202135dca40df4d095ca8b53
Gylychmyrat Mammedov <gylychmyratmammedov@Gylychmyrats-MacBook-Pro.local>
2020-12-30 23:56:22 -0500

    serverjs finilized

--- Server.js
+++ Server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const loader = require('morgan');
 const cors = require('cors');
+const booksRoutes  = require('./routes/books')
 const PORT = process.env.PORT || 3000;
-
 const app = express();
-app.get('/', (req,res) => {
-  res.send('HOME PAGE')
-})
+
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(logger('dev'))
+
+
+app.use('/library',booksRoutes)
 app.listen(PORT,()=>console.log(`Server is running on PORT :${PORT}`))

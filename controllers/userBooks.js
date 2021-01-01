0a73041323cd2b67bd1073ab4e4d7537717942d9
Gylychmyrat Mammedov <gylychmyratmammedov@Gylychmyrats-MacBook-Pro.local>
2021-01-01 14:36:12 -0500

    userBook file initial setUP

---
+++ controllers/userBooks.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user.js');
+const Book = require('../models/book.js')
+
+//GET api books
+//GEt user books
+//private
+
+const userBooks = () => {
+  
+}

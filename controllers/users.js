146f009b53248a938f288f71425a4411937e4ac2
Gylychmyrat Mammedov <gylychmyratmammedov@Gylychmyrats-MacBook-Pro.local>
2020-12-31 23:36:46 -0500

    user validation done

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,34 @@
 const express = require('express');
-const router = express.Router();
+const User = require('../models/user');
+const { body, validationResult } = require('express-validator');
+
 
 // POST library/users
 //Register a user
 // Public
-const userRegister = (req,res) => {
-  res.send('Register a user')
+const userRegister = async (req,res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({errors:errors.array()})
+  }
+  // Validation
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email: email });
+    if (user) {
+      return res.status(400).json({message:'User Alrady Exists'})
+    }
+    user = new User({
+      name,
+      email,
+      password
+    })
+    // For now, it is not saved to database. Before That we need to hash the password
+    const salt = await bcrypt.salt
+  } catch (error) {
+    
+  }
 }
 
 const getLoggedUser = ('/', () => {
--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,30 @@
-const { Router } = require('express')
-const BookControllers = require('../controllers/books');
-const UserControllers = require('../controllers/users')
+const { Router } = require("express");
+const BookControllers = require("../controllers/books");
+const UserControllers = require("../controllers/users");
+const { check, validationResult } = require("express-validator");
 
-const router = Router()
+const router = Router();
 
-router.get('/books', BookControllers.getBooks)
-router.get('/books/:id', BookControllers.getBook)
-router.post('/books', BookControllers.createBook)
-router.put('/books/:id', BookControllers.updateBook)
-router.delete('/books/:id', BookControllers.deleteBook)
+router.get("/books", BookControllers.getBooks);
+router.get("/books/:id", BookControllers.getBook);
+router.post("/books", BookControllers.createBook);
+router.put("/books/:id", BookControllers.updateBook);
+router.delete("/books/:id", BookControllers.deleteBook);
 
-router.post('/user', UserControllers.userRegister);
-router.get('/userLogin', UserControllers.userLogin);
-router.get('/currentUser',UserControllers.getLoggedUser)
+router.post(
+  "/user",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "Please Include a valid Email").isEmail(),
+    check("password", "Please Enter a password with 6 or more digits").isLength(
+      {
+        min: 6,
+      }
+    ),
+  ],
+  UserControllers.userRegister
+);
+router.get("/userLogin", UserControllers.userLogin);
+router.get("/currentUser", UserControllers.getLoggedUser);
 
-
-
-module.exports = router
+module.exports = router;
